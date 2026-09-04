cdd1a2a859829f9842b29e2aef554b1fddeb41fd
lil-fahad <73719703+lil-fahad@users.noreply.github.com>
2026-09-04 11:07:42 +0300

    rendering: expose public rendering foundation API

---
+++ src/furniture_ai/rendering/__init__.py
@@ -0,0 +1,29 @@
+from furniture_ai.rendering.contracts import (
+    CameraSpec,
+    RenderArtifact,
+    RenderPreviewRequest,
+    RenderPreviewResult,
+    RendererKind,
+    RenderPromptPackage,
+    SceneFurnitureItem,
+    SceneRoom,
+    SceneSpec,
+)
+from furniture_ai.rendering.prompt import PromptCompiler
+from furniture_ai.rendering.scene import SceneCompiler
+from furniture_ai.rendering.service import RenderingService
+
+__all__ = [
+    "CameraSpec",
+    "PromptCompiler",
+    "RenderArtifact",
+    "RenderPreviewRequest",
+    "RenderPreviewResult",
+    "RendererKind",
+    "RenderPromptPackage",
+    "RenderingService",
+    "SceneCompiler",
+    "SceneFurnitureItem",
+    "SceneRoom",
+    "SceneSpec",
+]
